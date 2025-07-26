4c84aaa7b212a07ba6fdc7edd44596e570e822ac
dannyelfloyd <dannyelfloyd@gmail.com>
2025-07-27 01:28:46 +0200

    improvements

---
+++ src/pages/NotesCreated.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NotesCreated = () => {
+  return (
+    <div>NotesCreated</div>
+  )
+}
+
+export default NotesCreated
---
+++ src/pages/NotesDetail.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NotesDetail = () => {
+  return (
+    <div>NotesDetail</div>
+  )
+}
+
+export default NotesDetail
